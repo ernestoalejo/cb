5571f9d5ccd9aaec0072707cc1862691c9ccecf2
Ernesto Alejo Oltra <ernestokarim@gmail.com>
2013-02-16 09:42:07 +0100

    Get tasks by name & version.

--- registry/registry.go
+++ registry/registry.go
@@ -8,6 +8,7 @@ type Task func(q *Queue) error
 
 var tasks = map[string]map[int]Task{}
 
+// Register a new task in the system
 func NewTask(name string, version int, f Task) {
 	m := tasks[name]
 	if m == nil {
@@ -17,6 +18,26 @@ func NewTask(name string, version int, f Task) {
 	tasks[name] = m
 }
 
+// Obtain the task by name and version. If version is -1 it will return the 
+// latest version of that task.
 func getTask(name string, version int) (Task, error) {
-	return nil, errors.Format("task not found: %s", name)
+	m := tasks[name]
+	if m == nil {
+		return nil, errors.Format("task not found: %s", name)
+	}
+
+	if version == -1 {
+		for v, _ := range m {
+			if v > version {
+				version = v
+			}
+		}
+	}
+
+	f := m[version]
+	if f == nil {
+		return nil, errors.Format("version not found: %d", version)
+	}
+
+	return f, nil
 }
