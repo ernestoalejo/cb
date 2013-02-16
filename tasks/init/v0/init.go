e583d48f7b85fbdcb6eef25aadd81f2a0c464e5d
Ernesto Alejo Oltra <ernestokarim@gmail.com>
2013-02-16 12:56:04 +0100

    Add templated files to the init task. Add app.yaml.

--- tasks/init/v0/init.go
+++ tasks/init/v0/init.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"text/template"
 
 	"github.com/ernestokarim/cb/config"
 	"github.com/ernestokarim/cb/errors"
@@ -14,7 +15,13 @@ import (
 	"github.com/ernestokarim/cb/utils"
 )
 
+// Pointer to this package (to locate the templates)
 const SELF_PKG = "github.com/ernestokarim/cb/tasks/init/v0/templates"
+
+// List of files that must be passed through the template system
+var needTemplates = map[string]bool{
+	"app.yaml": true,
+}
 
 func init() {
 	registry.NewTask("init", 0, init_task)
@@ -31,6 +38,8 @@ func init_task(c *config.Config, q *registry.Queue) error {
 	return copyFiles(filepath.Base(cur), base, cur)
 }
 
+// Copy recursively all the files in src to the dest folder. Appname will
+// be the name of the root folder, that gives the codename to the project.
 func copyFiles(appname, src, dest string) error {
 	files, err := ioutil.ReadDir(src)
 	if err != nil {
@@ -68,6 +77,8 @@ func copyFiles(appname, src, dest string) error {
 	return nil
 }
 
+// Copy a file, using templates if needed, from srcPath to destPath.
+// Rel is the relative-to-root-dest path.
 func copyFile(appname, srcPath, destPath, rel string) error {
 	_, err := os.Stat(destPath)
 	if err == nil {
@@ -81,21 +92,38 @@ func copyFile(appname, srcPath, destPath, rel string) error {
 		log.Printf("copy file %s\n", rel)
 	}
 
-	src, err := os.Open(srcPath)
-	if err != nil {
-		return errors.New(err)
-	}
-	defer src.Close()
-
 	dest, err := os.Create(destPath)
 	if err != nil {
 		return errors.New(err)
 	}
 	defer dest.Close()
 
-	if _, err := io.Copy(dest, src); err != nil {
-		return errors.New(err)
+	if needTemplates[rel] {
+		t, err := template.ParseFiles(srcPath)
+		if err != nil {
+			return errors.New(err)
+		}
+
+		if err := t.Execute(dest, getTemplateData(appname)); err != nil {
+			return err
+		}
+	} else {
+		src, err := os.Open(srcPath)
+		if err != nil {
+			return errors.New(err)
+		}
+		defer src.Close()
+
+		if _, err := io.Copy(dest, src); err != nil {
+			return errors.New(err)
+		}
 	}
 
 	return nil
+}
+
+func getTemplateData(appname string) map[string]interface{} {
+	return map[string]interface{}{
+		"AppName": appname,
+	}
 }
