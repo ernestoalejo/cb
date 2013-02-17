a99ef29068d88810b19c1379bf024db1c99cc9c4
Ernesto Alejo Oltra <ernestokarim@gmail.com>
2013-02-17 16:22:36 +0100

    Empty server task.

---
+++ tasks/server/v0/server.go
@@ -0,0 +1,16 @@
+package v0
+
+import (
+	"fmt"
+
+	"github.com/ernestokarim/cb/config"
+	"github.com/ernestokarim/cb/registry"
+)
+
+func init() {
+	registry.NewTask("server", 0, server)
+}
+
+func server(c *config.Config, q *registry.Queue) error {
+	return nil
+}
