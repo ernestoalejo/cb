895e335e631446fd17adc736ab9b0f6e40e33696
Ernesto Alejo Oltra <ernestokarim@gmail.com>
2013-02-24 23:24:57 +0100

    Create new controllers.

--- tasks/angular/v0/angular.go
+++ tasks/angular/v0/angular.go
@@ -40,10 +40,40 @@ describe('Service: {{ .Data.Name }}', function() {
   }));
 });
 `))
+	controllerTmpl = template.Must(template.New("controller").Parse(
+		`{{ if not .Exists }}'use strict';
+
+
+var m = angular.module('{{ .Data.Module }}', []);
+{{ end }}
+
+m.controller('{{ .Data.Name }}', function() {
+  // empty
+});
+`))
+	controllerTestTmpl = template.Must(template.New("controllerTest").Parse(
+		`{{ if not .Exists }}'use strict';
+
+{{ else }}
+{{ end }}
+describe('Controller: {{ .Data.Name }}', function() {
+  beforeEach(module('{{ .Data.Module }}'));
+
+  var scope;
+  beforeEach(inject(function($injector) {
+    var $controller = $injector.get('$controller');
+    var $rootScope = $injector.get('$rootScope');
+
+    scope = $rootScope.$new();
+    $controller('{{ .Data.Name }}', {$scope: scope});
+  }));
+});
+`))
 )
 
 func init() {
 	registry.NewTask("service", 0, service)
+	registry.NewTask("controller", 0, controller)
 }
 
 func service(c config.Config, q *registry.Queue) error {
@@ -69,6 +99,37 @@ func service(c config.Config, q *registry.Queue) error {
 	return nil
 }
 
+func controller(c config.Config, q *registry.Queue) error {
+	fmt.Printf(" - Name of the controller: ")
+	name, err := getLine()
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf(" - Module of the controller: ")
+	module, err := getLine()
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf(" - Route of the controller: ")
+	route, err := getLine()
+	if err != nil {
+		return err
+	}
+
+	if err := writeControllerFile(name, module, route); err != nil {
+		return err
+	}
+	if err := writeControllerTestFile(name, module, route); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// ==================================================================
+
 func getLine() (string, error) {
 	for {
 		line, err := buf.ReadString('\n')
@@ -82,26 +143,6 @@ func getLine() (string, error) {
 		}
 	}
 	panic("should not reach here")
-}
-
-type ServiceData struct {
-	Name, Module string
-}
-
-func writeServiceFile(name, module string) error {
-	parts := strings.Split(module, ".")
-	filename := parts[len(parts)-1] + ".js"
-	p := filepath.Join("client", "app", "scripts", "services", filename)
-
-	return writeFile(p, serviceTmpl, &ServiceData{name, module})
-}
-
-func writeServiceTestFile(name, module string) error {
-	parts := strings.Split(module, ".")
-	filename := parts[len(parts)-1] + "Spec.js"
-	p := filepath.Join("client", "test", "unit", "services", filename)
-
-	return writeFile(p, serviceTestTmpl, &ServiceData{name, module})
 }
 
 type FileData struct {
@@ -130,4 +171,48 @@ func writeFile(path string, tmpl *template.Template, data interface{}) error {
 	}
 
 	return nil
+}
+
+// ==================================================================
+
+type ServiceData struct {
+	Name, Module string
+}
+
+func writeServiceFile(name, module string) error {
+	parts := strings.Split(module, ".")
+	filename := parts[len(parts)-1] + ".js"
+	p := filepath.Join("client", "app", "scripts", "services", filename)
+
+	return writeFile(p, serviceTmpl, &ServiceData{name, module})
+}
+
+func writeServiceTestFile(name, module string) error {
+	parts := strings.Split(module, ".")
+	filename := parts[len(parts)-1] + "Spec.js"
+	p := filepath.Join("client", "test", "unit", "services", filename)
+
+	return writeFile(p, serviceTestTmpl, &ServiceData{name, module})
+}
+
+// ==================================================================
+
+type ControllerData struct {
+	Name, Module, Route string
+}
+
+func writeControllerFile(name, module, route string) error {
+	parts := strings.Split(module, ".")
+	filename := parts[len(parts)-1] + ".js"
+	p := filepath.Join("client", "app", "scripts", "controllers", filename)
+
+	return writeFile(p, controllerTmpl, &ControllerData{name, module, route})
+}
+
+func writeControllerTestFile(name, module, route string) error {
+	parts := strings.Split(module, ".")
+	filename := parts[len(parts)-1] + "Spec.js"
+	p := filepath.Join("client", "test", "unit", "controllers", filename)
+
+	return writeFile(p, controllerTestTmpl, &ControllerData{name, module, route})
 }
