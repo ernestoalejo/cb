f85699fd36c45b862405998638cabee3689db55e
Ernesto Alejo Oltra <ernestokarim@gmail.com>
2013-02-25 09:44:51 +0100

    Fix the build. Better feedback infrastructure, fix issue #3.

--- tasks/init/v0/templates/server/stuff/feedback.go
+++ tasks/init/v0/templates/server/stuff/feedback.go
@@ -1,7 +1,14 @@
 package stuff
 
 import (
+	"bytes"
+
+	"appengine"
+
+	"conf"
+
 	"github.com/ernestokarim/gaelib/app"
+	"github.com/ernestokarim/gaelib/mail"
 )
 
 type FeedbackData struct {
@@ -13,12 +20,38 @@ func Feedback(r *app.Request) error {
 	if err := r.LoadJsonData(data); err != nil {
 		return err
 	}
-
 	if data.Message == "" {
 		return app.Forbidden()
 	}
 
-	app.LogError(r.C, app.Errorf("%s", data.Message))
+	// Try to send an email to the admin if the app is in production
+	if !appengine.IsDevAppServer() {
+		appid := appengine.AppID(r.C)
+		for _, admin := range conf.ADMIN_EMAILS {
+			data := map[string]interface{}{
+				"Message":  data.Message,
+				"UserMail": admin,
+				"AppId":    appid,
+			}
+			html := bytes.NewBuffer(nil)
+			if err := app.Template(html, []string{"mails/feedback"}, data); err != nil {
+				return app.Error(err)
+			}
 
-	return r.EmitJson(map[string]string{})
+			m := &mail.Mail{
+				To:       admin,
+				ToName:   "Administrador",
+				From:     "feedback@" + appid + ".appspotmail.com",
+				FromName: "Feedback",
+				Subject:  "Mensaje del usuario",
+				Html:     string(html.Bytes()),
+			}
+			if err := mail.SendMail(r.C, m); err != nil {
+				return err
+			}
+		}
+	} else {
+		r.C.Errorf("FEEDBACK: %s", data.Message)
+	}
+	return nil
 }
