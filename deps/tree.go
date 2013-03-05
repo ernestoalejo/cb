1f7ee18010e9905280615f1025ed93f3be93c596
Ernesto Alejo Oltra <ernestokarim@gmail.com>
2013-03-05 12:21:31 +0100

    Implement the resolving of closure dependencies and writing them to a file.

--- deps/tree.go
+++ deps/tree.go
@@ -1,6 +1,9 @@
 package deps
 
 import (
+	"fmt"
+	"io"
+	"log"
 	"path/filepath"
 	"strings"
 
@@ -13,8 +16,17 @@ type Tree struct {
 	provides map[string]*Source
 	base     *Source
 
+	// List of dependencies we need to build the app,
+	// it should be explicitly computed once we have the list
+	// of namespaces we want to provide, calling ResolveDependencies()
+	deps []*Source
+
 	// Used temporary to build the tree
 	c config.Config
+
+	// Used temporary to build the list of namespaced
+	// when resolving dependencies and detect circular references.
+	traversal []string
 }
 
 // Creates a new dependencies tree based on the files inside several hard-coded
@@ -26,22 +38,9 @@ func NewTree(c config.Config) (*Tree, error) {
 		c:        c,
 	}
 
-	library, err := getLibraryRoot(c)
+	roots, err := baseJSPaths(t.c)
 	if err != nil {
 		return nil, err
-	}
-
-	templates, err := getTemplatesRoot(c)
-	if err != nil {
-		return nil, err
-	}
-
-	roots := []string{
-		library,
-		filepath.Join(library, "closure", "goog"),
-		"scripts",
-		filepath.Join("temp", "templates"),
-		filepath.Join(templates, "javascript", "soyutils_usegoog.js"),
 	}
 	for _, root := range roots {
 		if err := filepath.Walk(root, buildWalkFn(t)); err != nil {
@@ -106,6 +105,92 @@ func (t *Tree) addSource(path string) error {
 	return nil
 }
 
+func (t *Tree) PrintStats() {
+	if t.deps == nil {
+		log.Printf("depstree: %d files providing a total of %d namespaces\n",
+			len(t.sources), len(t.provides))
+	} else {
+		log.Printf("depstree: %d dependencies computed\n", len(t.deps))
+	}
+}
+
+// Returns the provides list of a source file, or an error if it hasn't been
+// scanned previously into the tree
+func (t *Tree) GetProvides(path string) ([]string, error) {
+	src, ok := t.sources[path]
+	if !ok {
+		return nil, errors.Format("input not present in the sources: `%s`", path)
+	}
+	return src.Provides, nil
+}
+
+func (t *Tree) ResolveDependencies(namespaces []string) error {
+	for _, ns := range namespaces {
+		if err := t.resolve(ns); err != nil {
+			return err
+		}
+	}
+	if len(t.traversal) > 0 {
+		return errors.Format("internal error: traversal should be empty")
+	}
+	return nil
+}
+
+func (t *Tree) resolve(namespace string) error {
+	src, ok := t.provides[namespace]
+	if !ok {
+		return errors.Format("namespace not found: `%s`", namespace)
+	}
+
+	if in(t.traversal, namespace) {
+		t.traversal = append(t.traversal, namespace)
+		return errors.Format("circular dependency: %v", t.traversal)
+	}
+
+	// Memoize results, if the source is already in the list of
+	// dependencies we don't have to add it again
+	if !inSources(t.deps, src) {
+		t.traversal = append(t.traversal, namespace)
+
+		for _, require := range src.Requires {
+			if err := t.resolve(require); err != nil {
+				return err
+			}
+		}
+		t.deps = append(t.deps, src)
+
+		t.traversal = t.traversal[:len(t.traversal)-1]
+	}
+	return nil
+}
+
+func (t *Tree) WriteDeps(f io.Writer) error {
+	paths, err := baseJSPaths(t.c)
+	if err != nil {
+		return err
+	}
+	for _, src := range t.deps {
+		provides := fmt.Sprintf("'%s'", strings.Join(src.Provides, "', '"))
+		requires := fmt.Sprintf("'%s'", strings.Join(src.Requires, "', '"))
+
+		var n string
+		for _, p := range paths {
+			tn, err := filepath.Rel(p, src.Path)
+			if err == nil && !strings.Contains(tn, "..") {
+				n = tn
+				break
+			}
+		}
+		if n == "" {
+			return errors.Format("cannot generate the relative path for `%s`",
+				src.Path)
+		}
+
+		fmt.Fprintf(f, "goog.addDependency('%s', [%s], [%s]);", n, provides, requires)
+	}
+	return nil
+}
+
 func in(lst []string, elem string) bool {
 	for _, item := range lst {
 		if item == elem {
@@ -115,143 +200,29 @@ func in(lst []string, elem string) bool {
 	return false
 }
 
-/*
-
-
-// Returns the provides list of a source file, or an error if it hasn't been
-// scanned previously into the tree
-func (tree *DepsTree) GetProvides(filename string) ([]string, error) {
-  src, ok := tree.sources[filename]
-  if !ok {
-    return nil, app.Errorf("input not present in the sources: %s", filename)
-  }
-
-  return src.Provides, nil
+func inSources(lst []*Source, s *Source) bool {
+	for _, v := range lst {
+		if v == s {
+			return true
+		}
+	}
+	return false
 }
 
-// Return the list of namespaces need to include the test files too
-func (tree *DepsTree) GetTestingNamespaces() []string {
-  ns := make([]string, 0)
-  for _, src := range tree.sources {
-    if strings.Contains(src.Filename, "_test.js") {
-      ns = append(ns, src.Provides...)
-    }
-  }
-  return ns
+func baseJSPaths(c config.Config) ([]string, error) {
+	library, err := getLibraryRoot(c)
+	if err != nil {
+		return nil, err
+	}
+	templates, err := getTemplatesRoot(c)
+	if err != nil {
+		return nil, err
+	}
+	return []string{
+		library,
+		filepath.Join(library, "closure", "goog"),
+		"scripts",
+		filepath.Join("temp", "templates"),
+		filepath.Join(templates, "javascript", "soyutils_usegoog.js"),
+	}, nil
 }
-
-// Struct to store the info of a dependencies tree traversal
-type TraversalInfo struct {
-  deps      []*domain.Source
-  traversal []string
-}
-
-// Returns the list of files (in order) that must be compiled to finally
-// obtain all namespaces, including the base one.
-func (tree *DepsTree) GetDependencies(namespaces []string) ([]*domain.Source, error) {
-  // Prepare the info
-  info := &TraversalInfo{
-    deps:      []*domain.Source{},
-    traversal: []string{},
-  }
-
-  for _, ns := range namespaces {
-    // Resolve all the needed dependencies
-    if err := tree.ResolveDependencies(ns, info); err != nil {
-      return nil, err
-    }
-  }
-
-  return info.deps, nil
-}
-
-// Adds to the traversal info the list of dependencies recursively.
-func (tree *DepsTree) ResolveDependencies(ns string, info *TraversalInfo) error {
-  // Check that the namespace is correct
-  src, ok := tree.provides[ns]
-  if !ok {
-    return app.Errorf("namespace not found: %s", ns)
-  }
-
-  // Detects circular deps
-  if In(info.traversal, ns) {
-    info.traversal = append(info.traversal, ns)
-    return app.Errorf("circular dependency detected: %v", info.traversal)
-  }
-
-  // Memoize results, don't recalculate old depencies
-  if !InSource(info.deps, src) {
-    // Add a new namespace to the traversal
-    info.traversal = append(info.traversal, ns)
-
-    // Compile first all dependencies
-    for _, require := range src.Requires {
-      tree.ResolveDependencies(require, info)
-    }
-
-    // Add ourselves to the list of files
-    info.deps = append(info.deps, src)
-
-    // Remove the namespace from the traversal
-    info.traversal = info.traversal[:len(info.traversal)-1]
-  }
-
-  return nil
-}
-
-func WriteDeps(f io.Writer, deps []*domain.Source) error {
-  paths := BaseJSPaths()
-  for _, src := range deps {
-    // Accumulates the provides & requires of the source
-    provides := "'" + strings.Join(src.Provides, "', '") + "'"
-    requires := "'" + strings.Join(src.Requires, "', '") + "'"
-
-    // Search the base path to the file, and put the path
-    // relative to it
-    var n string
-    for _, p := range paths {
-      tn, err := filepath.Rel(p, src.Filename)
-      if err == nil && !strings.Contains(tn, "..") {
-        n = tn
-        break
-      }
-    }
-    if n == "" {
-      return app.Errorf("cannot generate the relative filename for %s", src.Filename)
-    }
-
-    // Write the line to the output of the deps.js file request
-    fmt.Fprintf(f, "goog.addDependency('%s', [%s], [%s]);\n", n, provides, requires)
-  }
-
-  return nil
-}
-
-// Base paths, all routes to a JS must start from one
-// of these ones.
-// The order is important, the paths will be scanned as
-// they've been written.
-func BaseJSPaths() []string {
-  conf := config.Current()
-
-  p := []string{}
-
-  if conf.Library != nil {
-    p = append(p, path.Join(conf.Library.Root, "closure", "goog"))
-    p = append(p, conf.Library.Root)
-  }
-
-  if conf.Js != nil {
-    p = append(p, conf.Js.Root)
-  }
-
-  if conf.Soy != nil {
-    path.Join(conf.Soy.Compiler, "javascript")
-    if conf.Soy.Root != "" {
-      p = append(p, path.Join(conf.Build, "templates"))
-    }
-  }
-
-  return p
-}
-*/
--- tasks/closurejs/v0/closurejs.go
+++ tasks/closurejs/v0/closurejs.go
@@ -1,8 +1,12 @@
 package v0
 
 import (
+	"os"
+	"path/filepath"
+
 	"github.com/ernestokarim/cb/config"
 	"github.com/ernestokarim/cb/deps"
+	"github.com/ernestokarim/cb/errors"
 	"github.com/ernestokarim/cb/registry"
 )
 
@@ -15,6 +19,67 @@ func closurejs(c config.Config, q *registry.Queue) error {
 	if err != nil {
 		return err
 	}
-	_ = tree
+	if *config.Verbose {
+		tree.PrintStats()
+	}
+
+	inputs, err := getInputs(c)
+	if err != nil {
+		return err
+	}
+
+	namespaces := []string{}
+	for _, input := range inputs {
+		ns, err := tree.GetProvides(input)
+		if err != nil {
+			return err
+		}
+		namespaces = append(namespaces, ns...)
+	}
+	if len(namespaces) == 0 {
+		return errors.Format("no namespaces provided in the input files")
+	}
+
+	if err := tree.ResolveDependencies(namespaces); err != nil {
+		return err
+	}
+	if *config.Verbose {
+		tree.PrintStats()
+	}
+
+	f, err := os.Create(filepath.Join("temp", "deps.js"))
+	if err != nil {
+		return errors.New(err)
+	}
+	defer f.Close()
+
+	if err := tree.WriteDeps(f); err != nil {
+		return err
+	}
+
 	return nil
+}
+
+func getInputs(c config.Config) ([]string, error) {
+	if c["closurejs"] == nil {
+		return nil, errors.Format("`closurejs` configurations required")
+	}
+	if c["closurejs"]["inputs"] == nil {
+		return nil, errors.Format("`closurejs.inputs` configurations required")
+	}
+
+	rawInputs, ok := c["closurejs"]["inputs"].([]interface{})
+	if !ok {
+		return nil, errors.Format("`closurejs.inputs` should be a list")
+	}
+
+	inputs := []string{}
+	for _, input := range rawInputs {
+		s, ok := input.(string)
+		if !ok {
+			return nil, errors.Format("`closurejs.inputs` elements should be strings")
+		}
+		inputs = append(inputs, s)
+	}
+	return inputs, nil
 }
