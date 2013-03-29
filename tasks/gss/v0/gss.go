5fba6ecc579b53d159482c4458641ad29226dec3
Ernesto Alejo Oltra <ernestokarim@gmail.com>
2013-03-29 11:09:33 +0100

    Fix gss task. Add a YAML config for the closure mode.

--- tasks/gss/v0/gss.go
+++ tasks/gss/v0/gss.go
@@ -20,28 +20,35 @@ func gss(c *config.Config, q *registry.Queue) error {
 		return fmt.Errorf("closure mode only task")
 	}
 
-	compilerPath, err := getCompilerPath(c)
+	compilerPath, err := c.GetString("closure.stylesheets")
 	if err != nil {
 		return fmt.Errorf("cannot get compiler path: %s", err)
 	}
-	files, err := gssFromConfig(c)
+	size, err := c.Count("gss")
 	if err != nil {
-		return fmt.Errorf("read config failed: %s", err)
+		return fmt.Errorf("count gss files failed: %s", err)
 	}
+	for i := 0; i < size; i++ {
+		src, err := c.GetStringf("gss[%d].source", i)
+		if err != nil {
+			return fmt.Errorf("get gss source failed: %s", err)
+		}
+		dest, err := c.GetStringf("gss[%d].dest", i)
+		if err != nil {
+			return fmt.Errorf("get gss dest failed: %s", err)
+		}
 
-	for _, file := range files {
-		dir := filepath.Dir(file.dest)
+		dir := filepath.Dir(dest)
 		if err := os.MkdirAll(dir, 0755); err != nil {
 			return fmt.Errorf("create dest dir failed (%s): %s", dir, err)
 		}
-
 		args := []string{
 			"-jar", compilerPath,
 			"--output-renaming-map-format", "CLOSURE_COMPILED",
 			"--rename", "CLOSURE",
 			"--output-renaming-map", filepath.Join("temp", "gssmap.js"),
-			"--output-file", file.dest,
-			file.src,
+			"--output-file", dest,
+			src,
 		}
 		output, err := utils.Exec("java", args)
 		if err != nil {
@@ -49,44 +56,9 @@ func gss(c *config.Config, q *registry.Queue) error {
 			return fmt.Errorf("compiler error: %s", err)
 		}
 		if *config.Verbose {
-			log.Printf("created file %s\n", file.dest)
+			log.Printf("created file %s\n", dest)
 		}
 	}
 
 	return nil
-}
-
-type gssFile struct {
-	src, dest string
-}
-
-func gssFromConfig(c *config.Config) ([]*gssFile, error) {
-	files := []*gssFile{}
-	for dest, rawsrc := range c["gss"] {
-		src, ok := rawsrc.(string)
-		if !ok {
-			return nil, fmt.Errorf("`gss` config should be a map[string]string")
-		}
-
-		src = filepath.Join("temp", src)
-		dest = filepath.Join("temp", "styles", dest)
-		files = append(files, &gssFile{src, dest})
-	}
-	return files, nil
-}
-
-// Compute the compiler path from the config settings and return it
-func getCompilerPath(c *config.Config) (string, error) {
-	if c["closure"] == nil {
-		return "", fmt.Errorf("`closure` config required")
-	}
-	if c["closure"]["stylesheets"] == nil {
-		return "", fmt.Errorf("`closure.stylesheets` config required")
-	}
-	s, ok := c["closure"]["stylesheets"].(string)
-	if !ok {
-		return "", fmt.Errorf("`closure.stylesheets` should be a string")
-	}
-	s = filepath.Join(s, "build", "closure-stylesheets.jar")
-	return s, nil
 }
