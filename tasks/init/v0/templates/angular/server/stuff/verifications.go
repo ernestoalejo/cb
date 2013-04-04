2528ec3c05121d2ae7b2f704a9732a43fb9d1f03
Ernesto Alejo Oltra <ernestokarim@gmail.com>
2013-04-04 08:58:45 +0200

    Fix Bing site authentication.

--- tasks/init/v0/templates/angular/server/stuff/verifications.go
+++ tasks/init/v0/templates/angular/server/stuff/verifications.go
@@ -1,6 +1,8 @@
 package stuff
 
 import (
+	"html/template"
+
 	"github.com/ernestokarim/gaelib/v1/app"
 	"github.com/gorilla/mux"
 
@@ -20,7 +22,10 @@ func GoogleVerification(r *app.Request) error {
 
 func BingVerification(r *app.Request) error {
 	if conf.BING_VERIFICATION != "" {
-		d := map[string]interface{}{"Id": conf.BING_VERIFICATION}
+		d := map[string]interface{}{
+			"Id": conf.BING_VERIFICATION,
+			"Lt": template.HTML("<"),
+		}
 		return r.Template([]string{"verifications/bing"}, d)
 	}
 	return app.NotFound()
