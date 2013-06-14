6151d5b152bea2976d180685623e231728462903
Ernesto Alejo Oltra <ernestokarim@gmail.com>
2013-06-14 17:08:41 +0200

    Gofmt.

--- tasks/deploy/v0/deploy.go
+++ tasks/deploy/v0/deploy.go
@@ -1,12 +1,12 @@
 package v0
 
 import (
+	"crypto/sha1"
 	"fmt"
+	"io/ioutil"
+	"os"
 	"path/filepath"
 	"strings"
-  "os"
-  "io/ioutil"
-  "crypto/sha1"
 
 	"github.com/ernestokarim/cb/config"
 	"github.com/ernestokarim/cb/registry"
@@ -38,9 +38,9 @@ var (
     `,
 	}
 
-  macros = map[string]Macro{
-    "copyModTimes": copyModTimes,
-  }
+	macros = map[string]Macro{
+		"copyModTimes": copyModTimes,
+	}
 )
 
 func init() {
@@ -60,17 +60,17 @@ func deploy(c *config.Config, q *registry.Queue) error {
 			continue
 		}
 
-    // Execute macros
-    if command[0] == '@' && macros[command[1:]] != nil {
-      var err error
-      command, err = macros[command[1:]]()
-      if err != nil {
-        return fmt.Errorf("macro %s failed: %s", command[1:], err)
-      }
-      if len(command) == 0 {
-        continue
-      }
-    }
+		// Execute macros
+		if command[0] == '@' && macros[command[1:]] != nil {
+			var err error
+			command, err = macros[command[1:]]()
+			if err != nil {
+				return fmt.Errorf("macro %s failed: %s", command[1:], err)
+			}
+			if len(command) == 0 {
+				continue
+			}
+		}
 
 		// Replace some variables in the commands
 		command = strings.Replace(command, "$basename", basename, -1)
@@ -87,62 +87,62 @@ func deploy(c *config.Config, q *registry.Queue) error {
 }
 
 func copyModTimes() (string, error) {
-  walkFn := func(path string, info os.FileInfo, err error) error {
-    if err != nil {
-      return fmt.Errorf("recursive walk failed: %s", err)
-    }
-    if info.IsDir() {
-      return nil
-    }
+	walkFn := func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return fmt.Errorf("recursive walk failed: %s", err)
+		}
+		if info.IsDir() {
+			return nil
+		}
 
-    newPath := filepath.Join("temp", path[2:])
-    if _, err := os.Stat(newPath); err != nil {
-      if os.IsNotExist(err) {
-        return nil
-      }
-      return fmt.Errorf("stat error: %s", err)
-    }
+		newPath := filepath.Join("temp", path[2:])
+		if _, err := os.Stat(newPath); err != nil {
+			if os.IsNotExist(err) {
+				return nil
+			}
+			return fmt.Errorf("stat error: %s", err)
+		}
 
-    h1, err := hashFile(path)
-    if err != nil {
-      return fmt.Errorf("first hash failed: %s", err)
-    }
-    h2, err := hashFile(newPath)
-    if err != nil {
-      return fmt.Errorf("second hash failed: %s", err)
-    }
+		h1, err := hashFile(path)
+		if err != nil {
+			return fmt.Errorf("first hash failed: %s", err)
+		}
+		h2, err := hashFile(newPath)
+		if err != nil {
+			return fmt.Errorf("second hash failed: %s", err)
+		}
 
-    if h1 == h2 {
-      if err := os.Chtimes(newPath, info.ModTime(), info.ModTime()); err != nil {
-        return fmt.Errorf("change times failed: %s", err)
-      }
-    }
-    
-    return nil
-  }
-  if err := filepath.Walk("../deploy", walkFn); err != nil {
-    return "", fmt.Errorf("walk failed: %s", err)
-  }
+		if h1 == h2 {
+			if err := os.Chtimes(newPath, info.ModTime(), info.ModTime()); err != nil {
+				return fmt.Errorf("change times failed: %s", err)
+			}
+		}
 
-  return "", nil
+		return nil
+	}
+	if err := filepath.Walk("../deploy", walkFn); err != nil {
+		return "", fmt.Errorf("walk failed: %s", err)
+	}
+
+	return "", nil
 }
 
 func hashFile(path string) (string, error) {
-  f, err := os.Open(path)
-  if err != nil {
-    return "", fmt.Errorf("open failed: %s", err)
-  }
-  defer f.Close()
+	f, err := os.Open(path)
+	if err != nil {
+		return "", fmt.Errorf("open failed: %s", err)
+	}
+	defer f.Close()
 
-  content, err := ioutil.ReadAll(f)
-  if err != nil {
-    return "", fmt.Errorf("read failed: %s", err)
-  }
+	content, err := ioutil.ReadAll(f)
+	if err != nil {
+		return "", fmt.Errorf("read failed: %s", err)
+	}
 
-  h := sha1.New()
-  if _, err := h.Write(content); err != nil {
-    return "", fmt.Errorf("write failed: %s", err)
-  }
+	h := sha1.New()
+	if _, err := h.Write(content); err != nil {
+		return "", fmt.Errorf("write failed: %s", err)
+	}
 
-  return fmt.Sprintf("%x", h.Sum(nil)), nil
+	return fmt.Sprintf("%x", h.Sum(nil)), nil
 }
