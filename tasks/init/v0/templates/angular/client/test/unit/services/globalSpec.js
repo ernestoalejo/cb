706b96d547415fa8c94458e6e8402a87b1bd502a
Ernesto Alejo Oltra <ernestokarim@gmail.com>
2013-06-16 02:51:37 +0200

    Simplified global test.

--- tasks/init/v0/templates/angular/client/test/unit/services/globalSpec.js
+++ tasks/init/v0/templates/angular/client/test/unit/services/globalSpec.js
@@ -10,42 +10,14 @@ describe('GlobalMsg tests', function() {
     $timeout = $injector.get('$timeout');
   }));
 
-  it('should save the message', function() {
-    GlobalMsg.set('testing');
-    expect(GlobalMsg.get()).toBe('testing');
-  });
+  it('should save the last alert', function() {
+    GlobalMsg.create('one before', 'success');
+    expect(GlobalMsg.getLast().msg).toBe('one before');
+    expect(GlobalMsg.getLast().type).toBe('success');
 
-  it('should hide temp messages', function() {
-    GlobalMsg.setTemp('foo');
-    $timeout.flush();
-    expect(GlobalMsg.get()).toBe('');
-  });
-
-  it('should stop old hiding actions on set', function() {
-    GlobalMsg.setTemp('foo');
-    GlobalMsg.set('bar');
-
-    var called = 0;
-    try {
-      $timeout.flush();
-    } catch (err) {
-      called++;
-      expect(err.message).toBe('No deferred tasks to be flushed');
-    }
-    expect(called).toBe(1);
-    expect(GlobalMsg.get()).toBe('bar');
-  });
-
-  it('should store the class correctly', function() {
-    GlobalMsg.set('foo', 'error');
-    expect(GlobalMsg.getClass()).toBe('label-error');
-    GlobalMsg.set('foo');
-    expect(GlobalMsg.getClass()).toBe('label-success');
-
-    GlobalMsg.setTemp('foo', 'error');
-    expect(GlobalMsg.getClass()).toBe('label-error');
-    GlobalMsg.setTemp('foo');
-    expect(GlobalMsg.getClass()).toBe('label-success');
+    GlobalMsg.create('the last one', 'info');
+    expect(GlobalMsg.getLast().msg).toBe('the last one');
+    expect(GlobalMsg.getLast().type).toBe('info');
   });
 });
 
